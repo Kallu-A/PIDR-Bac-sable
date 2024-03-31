d73f7e243ed3470cd27242f3aadd0186d754ca34
ZHEN Julie <julie.zhen@telecomnancy.eu>
2024-03-31 22:04:57 +0200

    fix : discretisation image

--- src/real_wold.py
+++ src/real_wold.py
@@ -1,7 +1,12 @@
 from global_var import get_path_find
+import sys
 
+cellsX = 8
+cellsY = 10
+pixelsX = 100
+pixelsY = 100
 
-# convert the case i,j (in meters) in coordinate x,y in the image (pixel)
+# convert the case i,j (in meters) in coordinate (middle pixel) x,y in the image (pixel)
 def convert_meters_to_pixels(i, j, scaleX, scaleY):
     # scaleX = realMeasureX / number of pixels X (m/px)
     # scaleY = realMeasureY / number of pixels Y (m/px)
@@ -27,6 +32,119 @@ def convert_case_to_pixel(i, j):
 
 
 # convert the pixel x,y in the image to case i,j
-def convert_pixel_to_case(x, j):
-    return x, j
-    # TODO
+def convert_pixel_to_case(x, y):
+    tabX = discretization_X(cellsX, pixelsX)
+    tabY = discretization_Y(cellsY, pixelsY)
+    i = 0
+    j = 0
+    if x >= pixelsX:
+        print("Pixel x not in the image")
+        return
+
+    if y >= pixelsY:
+        print("Pixel y not in the image")
+        return
+
+    while not tabX[i] <= x < tabX[i + 1]:
+        i += 1
+        if x >= tabX[i] and tabX[i] == tabX[-1]:
+            break
+
+    while not tabY[j] <= y < tabY[j + 1]:
+        j += 1
+        if y >= tabY[j] and tabY[j] == tabY[-1]:
+            break
+
+    return i, j
+
+
+def discretization_X(cellsX, pixelsX):
+    # cellsX : number of cells wanted for X axis
+    # pixelsX : number of pixels for the X axis (horizontal)
+    tabX = [0] # will contain indexes of the first pixel of separation between cells, for X axis
+    if cellsX > pixelsX:
+        print("discretization error : cellsX > pixelsX")
+        return
+
+    if cellsX == 0:
+        print("The number of cells on X axis can't be 0")
+        return
+
+    if pixelsX == 0:
+        print("The number of pixels on X axis can't be 0")
+        return
+
+    if cellsX == pixelsX:
+        return [i for i in range(cellsX)]
+
+    pixNb = 0
+    for i in range(cellsX):
+        pixNb += pixelsX // cellsX
+        if pixNb != pixelsX:
+            tabX.append(pixNb)
+
+    if pixelsX - tabX[-1] < cellsX:
+        toAdd = 0
+        for i in range(len(tabX)):
+            tabX[i] += toAdd
+            if toAdd < (pixelsX - tabX[-1]):
+                toAdd += 1
+    if tabX[-1] == pixelsX:
+        tabX.pop()
+
+    return tabX
+
+
+def discretization_Y(cellsY, pixelsY):
+    # cellsY : number of cells wanted for Y axis
+    # pixelsY : number of pixels for the Y axis (horizontal)
+    tabY = [0]  # will contain indexes of the first pixel of separation between cells, for X axis
+    if cellsY > pixelsY:
+        print("discretization error : cellsY > pixelsY")
+        return
+
+    if cellsY == 0:
+        print("The number of cells on Y axis can't be 0")
+        return
+
+    if pixelsY == 0:
+        print("The number of pixels on Y axis can't be 0")
+        return
+
+    if cellsY == pixelsY:
+        return [i for i in range(cellsY)]
+
+    pixNb = 0
+    for i in range(cellsY):
+        pixNb += pixelsY // cellsY
+        if pixNb != pixelsY:
+            tabY.append(pixNb)
+
+    if pixelsY - tabY[-1] < cellsY:
+        toAdd = 0
+        for i in range(len(tabY)):
+            tabY[i] += toAdd
+            if toAdd < (pixelsY - tabY[-1]):
+                toAdd += 1
+    if tabY[-1] == pixelsY:
+        tabY.pop()
+    return tabY
+
+
+def discretization_table(cellsX, cellsY, pixelsX, pixelsY):
+    # return the discretization grid initialized with 0
+    tabX = discretization_X(cellsX, pixelsX)
+    tabY = discretization_Y(cellsY, pixelsY)
+    nbCellsX = len(tabX)
+    nbCellsY = len(tabY)
+    table = [[0 for col in range(nbCellsX)] for row in range(nbCellsY)]
+    return table
+
+
+if __name__ == "__main__":
+     tabX = discretization_X(cellsX, pixelsX)
+     tabY = discretization_Y(cellsY, pixelsY)
+     print(tabX)
+     print(tabY)
+     print(discretization_table(len(tabX), len(tabY), pixelsX, pixelsY))
+     print(convert_pixel_to_case(99, 90))
