4ea9a243eae4f10eabf2e84083c6d4f718737065
demure1u <aurelie.demure@telecomnancy.eu>
2024-04-17 00:07:58 +0200

    adaptation des fonctions de démo pour linux

--- src/calibration/testCalibration.py
+++ src/calibration/testCalibration.py
@@ -4,45 +4,52 @@ import numpy as np
 import cv2
 import os
 
-taille_damier = (6,5)
 
-objp = np.zeros((taille_damier[0]*taille_damier[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:taille_damier[0], 0:taille_damier[1]].T.reshape(-1,2)
+def calibrate():
+    taille_damier = (6,5)
 
-objpoints = []
-imgpoints = []
+    objp = np.zeros((taille_damier[0]*taille_damier[1], 3), np.float32)
+    objp[:,:2] = np.mgrid[0:taille_damier[0], 0:taille_damier[1]].T.reshape(-1,2)
 
-dossierCalibration = "src/calibration/images/"
-nomImages = [f for f in os.listdir(dossierCalibration) if f.endswith('.png')]
-nomImages.sort()
-print(nomImages)
-images = [os.path.join(dossierCalibration, image) for image in nomImages]
-print(images)
+    objpoints = []
+    imgpoints = []
 
-for nom in images : 
-    print(nom)
-    img = cv2.imread(nom)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    dossierCalibration = "src/calibration/images/"
+    nomImages = [f for f in os.listdir(dossierCalibration) if f.endswith('.png')]
+    nomImages.sort()
+    print(nomImages)
+    images = [os.path.join(dossierCalibration, image) for image in nomImages]
+    print(images)
+
+    for nom in images : 
+        print(nom)
+        img = cv2.imread(nom)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    retour, coins = cv2.findChessboardCorners(gray, taille_damier, None)
+        retour, coins = cv2.findChessboardCorners(gray, taille_damier, None)
     
-    if retour == True :
-        objpoints.append(objp)
-        imgpoints.append(coins)
+        if retour == True :
+            objpoints.append(objp)
+            imgpoints.append(coins)
         
-        img = cv2.drawChessboardCorners(img, taille_damier, coins, retour)
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-    else:
-        print("nope")
+            img = cv2.drawChessboardCorners(img, taille_damier, coins, retour)
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
+        else:
+            print("nope")
 
-retour, matrice, distorsion, rvec, tvec = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    retour, matrice, distorsion, rvec, tvec = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print(matrice)
-print(distorsion)
+    print(matrice)
+    print(distorsion)
 
-np.save('2cameraMatrice.npy', matrice)
-np.save('2distorsionVecteur.npy', distorsion)
+    np.save('2cameraMatrice.npy', matrice)
+    np.save('2distorsionVecteur.npy', distorsion)
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
+def main():
+    calibrate()
+
+if __name__ == '__main__':
+    main()
