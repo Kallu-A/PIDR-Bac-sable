11c83c3c50e462a54b30aac3bb92345e96ab3755
demure1u <aurelie.demure@telecomnancy.eu>
2024-05-08 12:25:05 +0200

    init de la fonction du suivi de trajectoire

---
+++ src/trajectoryTracking/trajectoryTracking.py
@@ -0,0 +1,36 @@
+from utility.movement_control import get_rotate_needed, rotate_robot
+from robot import Robot
+
+
+""" 
+We need a function that will take the trajectory given
+by the dStarLite3d code and follow it using Thymio functions.
+""" 
+
+def follow_trajectory(pathx, pathy, orientations):
+    
+    robot = Robot()
+    currentOrientation = robot.getOrientation()
+    
+    """
+    The trajectory will be a list of x_positions, y_positions and orientations.
+    """
+    
+    for x, y, orientation in zip(pathx, pathy, orientations):
+        rotate_robot(currentOrientation, orientation)
+        currentOrientation = orientation
+        
+        robot.move_robot(robot,x,y)
+    
+    """
+    Once we have the coordinates one by one, we have to follow them.
+    We first get the orientation needed,
+    and then go the the x,y position.
+    """
+    
+    return()
+
+
+    
+        
+        
