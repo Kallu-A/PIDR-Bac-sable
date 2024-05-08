5d7e9a13b1a35551d818c43d06275144113cff90
ZHEN Julie <julie.zhen@telecomnancy.eu>
2024-05-08 20:49:19 +0200

    3D

--- src/dStarLite3d.py
+++ src/dStarLite3d.py
@@ -43,21 +43,22 @@ def add_coordinates(node1: Node, node2: Node):
 
 
 def compare_coordinates(node1: Node, node2: Node):
-    return node1.x == node2.x and node1.y == node2.y
+    return node1.x == node2.x and node1.y == node2.y and node1.theta == node2.theta
 
 
 class DStarLite:
     # Please adjust the heuristic function (h) if you change the list of
     # possible motions
     motions = [
-        Node(1, 0, 1),
-        Node(0, 1, 1),
-        Node(-1, 0, 1),
-        Node(0, -1, 1),
-        Node(1, 1, math.sqrt(2)),
-        Node(1, -1, math.sqrt(2)),
-        Node(-1, 1, math.sqrt(2)),
-        Node(-1, -1, math.sqrt(2))
+        Node(1, 0, 0, 1),
+        Node(0, 1, 0, 1),
+        Node(-1, 0, 0, 1),
+        Node(0, -1, 0, 1),
+        Node(1, 1, 0, math.sqrt(2)),
+        Node(1, -1, 0, math.sqrt(2)),
+        Node(-1, 1, 0, math.sqrt(2)),
+        Node(-1, -1, 0, math.sqrt(2))
+
     ]
 
     def __init__(self, ox: list, oy: list):
